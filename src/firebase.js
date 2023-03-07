eefc79d14fbb6e86f1ad80fcd0a05af3336997f6
Nilni Kamburugamuwa <ms.klana.kam@gmail.com>
2023-03-07 22:49:25 +0530

    initial mass commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDALfa1vm804HG89jKhFfCXYTFK2AaIW40",
+  authDomain: "challenge-5041f.firebaseapp.com",
+  projectId: "challenge-5041f",
+  storageBucket: "challenge-5041f.appspot.com",
+  messagingSenderId: "586785036164",
+  appId: "1:586785036164:web:ef97e07e79e525b57befd1",
+  measurementId: "G-QS04WFETFP"
+};
